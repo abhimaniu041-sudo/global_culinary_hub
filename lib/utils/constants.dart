76e08281801c1c4e7a1773a8039dee22e4976431
abhimaniu041-sudo <abhimaniu041@gmail.com>
2026-05-29 23:08:20 +0530

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,30 @@
+class AppConstants {
+  static const String favoritesBoxName = 'favorites';
+  static const String historyBoxName = 'history';
+  static const String offlineBoxName = 'offline_recipes';
+  static const String cacheBoxName = 'ai_cache';
+  static const String settingsBoxName = 'settings';
+
+  static const String themeModeKey = 'theme_mode';
+  static const String languageKey = 'preferred_language';
+
+  static const List<String> cuisineCategories = [
+    'Italian',
+    'Indian',
+    'Chinese',
+    'Japanese',
+    'Mexican',
+    'French',
+    'Thai',
+    'American',
+    'Mediterranean',
+    'Middle Eastern',
+    'Greek',
+    'Spanish',
+    'Korean',
+    'Vietnamese',
+    'Ethiopian',
+  ];
+
+  static const List<String> difficulties = ['Easy', 'Medium', 'Hard'];
+}
